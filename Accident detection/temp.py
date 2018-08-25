ff218edb405f456a1037ba4ed4a4996de5d1c147
miscsamchris <misc.samchris@gmail.com>
2018-08-25 21:26:39 +0530

    8

--- Accident detection/temp.py
+++ Accident detection/temp.py
@@ -3,12 +3,7 @@ import cv2
 import math, operator
 import functools
 def sendmail():
-    print("TODO Send mail")
-cascade_src = 'cars.xml'
-video_src = 'C:/Users/Sam Christian/Desktop/Guvi Hack/traffic.mkv'
-cap = cv2.VideoCapture(video_src)
-car_cascade = cv2.CascadeClassifier(cascade_src)
-
+        print("Accident: Send message to Control Room")
 
 #Function to find difference in frames
 def diffImg(t0, t1, t2):
@@ -28,25 +23,22 @@ t_minus = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
 t = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
 t_plus = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
 cv2.imwrite("C:/Users/Sam Christian/Desktop/Guvi Hack/shotp.jpg",t)
+cascade_src = 'cars.xml'
+bus_src='Bus_front.xml'
+motorbike='two_wheeler.xml'
 
+video_src = 'C:/Users/Sam Christian/Desktop/Guvi Hack/road.mp4'
+    
+cap = cv2.VideoCapture(video_src)
+car_cascade = cv2.CascadeClassifier(cascade_src)
+bus_cascade = cv2.CascadeClassifier(bus_src)
+mb_cascade = cv2.CascadeClassifier(motorbike)
 while True:
   #Display video out through the window we created
-#  cv2.imshow( winName, diffImg(t_minus, t, t_plus) )
-#  cv2.imshow("Video",t)
+  cv2.imshow( winName, diffImg(t_minus, t, t_plus) )
+  cv2.imshow("Video",t)
   #Calling function diffImg() and assign the return value to 'p'
   p=diffImg(t_minus, t, t_plus)
-  ret, img = cap.read()
-  if (type(img) == type(None)):
-        break
-    
-  gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    
-  cars = car_cascade.detectMultiScale(gray, 1.1, 1)
-
-  for (x,y,w,h) in cars:
-        cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)      
-    
-  cv2.imshow('video', img)
   
   #Writing 'p' to a directory
   cv2.imwrite("C:/Users/Sam Christian/Desktop/Guvi Hack/shot.jpg",p)
@@ -69,12 +61,28 @@ while True:
   t_minus = t
   t = t_plus
   t_plus = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
-  
+  ret, img = cap.read()
+  if (type(img) == type(None)):
+        break
+    
+  gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+  cars = car_cascade.detectMultiScale(gray, 1.1, 1)
+  buses = car_cascade.detectMultiScale(gray, 1.1, 1)
+  bikes = car_cascade.detectMultiScale(gray, 1.1, 1)
+
+  for (x,y,w,h) in cars:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)  
+  for (x,y,w,h) in buses:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)   
+  for (x,y,w,h) in bikes:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)   
+        
+  cv2.imshow('video', img)
   #Destroys the window after key press
   key = cv2.waitKey(10)
   if key == 27:
-    cv2.destroyWindow(winName)
-    cv2.destroyWindow("Video")
-    cv2.destroyWindow("video")
-
+#    cv2.destroyWindow(winName)
+#    cv2.destroyWindow("Video")
+      cv2.destroyAllWindows()
     break  
--- Accident detection/testcardetect.py
+++ Accident detection/testcardetect.py
@@ -4,11 +4,15 @@ import cv2
 print(cv2.__version__)
 
 cascade_src = 'cars.xml'
-video_src = 'C:/Users/Sam Christian/Desktop/Guvi Hack/traffic.mkv'
-#video_src = 'dataset/video2.avi'
+bus_src='Bus_front.xml'
+motorbike='two_wheeler.xml'
 
+video_src = 'C:/Users/Sam Christian/Desktop/Guvi Hack/road.mp4'
+    
 cap = cv2.VideoCapture(video_src)
 car_cascade = cv2.CascadeClassifier(cascade_src)
+bus_cascade = cv2.CascadeClassifier(bus_src)
+mb_cascade = cv2.CascadeClassifier(motorbike)
 
 while True:
     ret, img = cap.read()
@@ -18,10 +22,16 @@ while True:
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     
     cars = car_cascade.detectMultiScale(gray, 1.1, 1)
+    buses = car_cascade.detectMultiScale(gray, 1.1, 1)
+    bikes = car_cascade.detectMultiScale(gray, 1.1, 1)
 
     for (x,y,w,h) in cars:
-        cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)      
-    
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)  
+    for (x,y,w,h) in buses:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)   
+    for (x,y,w,h) in bikes:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)   
+        
     cv2.imshow('video', img)
     
     if cv2.waitKey(33) == 27:
